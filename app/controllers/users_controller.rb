597524df06eba06155c537726e24b329f347a355
Bojan Ivanović <bojan.ivanovic6@gmail.com>
2017-03-04 12:31:32 +0100

    Break

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-  log_in @user
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # handle a successful update
+    else
+      render 'edit'
     end
   end
 
